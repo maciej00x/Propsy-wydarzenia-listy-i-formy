ede148d836e89f258539409050eb1964e23e9d0a
unknown <m.zawadzki022@gmail.com>
2024-01-30 18:16:21 +0100

    zadanie

---
+++ lekcja_2/src/App.js
@@ -0,0 +1,15 @@
+import styles from "./App.module.scss";
+import Form from "./components/Form/Form";
+
+function App() {
+  return (
+    <div className="container">
+      <section className={styles.section}>
+        <h2>Wprowadź swoje dane:</h2>
+        <Form />
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ lekcja_2/src/components/Form/Form.jsx
@@ -0,0 +1,21 @@
+import styles from "./Form.module.scss";
+
+const Form = () => {
+  const handleNameChange = (e) => {
+    console.log(e.target.value);
+  };
+  return (
+    <form className={styles.form}>
+      <div className={styles.formGroup}>
+        <label htmlFor="name">Name:</label>
+        <input onChange={handleNameChange} type="text" name="name" />
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor="lastName">Last name:</label>
+        <input type="text" name="lastName" />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
